ab0f9b449ad9b309fa65fdec9bae219f1cabd7ce
Ross Hill <ross.hill@nucleusfinancial.com>
2019-08-02 16:09:26 +0100

    Basic site created

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,17 +2,15 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to my new Gatsby site.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+    {/* <p>Welcome to my new Gatsby site.</p> */}
+    <h1 id="title">Test</h1>
       {/* <Image /> */}
-    </div>
+      <div class="type1"></div>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
