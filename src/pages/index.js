9206e84fc6fc2c8040e98220fc685963edbc58de
Ross Hill <ross.hill@nucleusfinancial.com>
2019-08-06 11:52:30 +0100

    Removed default page 2

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,20 +9,21 @@ const IndexPage = () => (
     <SEO title="Home" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/simple-line-icons/2.4.1/css/simple-line-icons.css" />
     {/* <p>Welcome to my new Gatsby site.</p> */}
-    <h1 id="title">Test</h1>
+    <h1 id="title">Ross Hill</h1>
+    <h2>Web Developer</h2>
+    <h2>Drupal Site-Builder</h2>
       {/* <Image /> */}
       <div class="type1">
         <div class="social-row">
-        <a href="https://github.com/rossghill"><i class="icon-social-github icons"></i></a>
-        <a href="https://www.linkedin.com/in/rossghill/"><i class="icon-social-linkedin icons"></i></a>
+          <a class="coral-icon" target="_blank" href="https://github.com/rossghill"><i class="icon-social-github icons"></i></a>
+          <a class="aqua-icon" target="_blank" href="https://www.linkedin.com/in/rossghill/"><i class="icon-social-linkedin icons"></i></a>
       </div> 
         <div class="social-row">
-        <a href="https://www.instagram.com/raspylines/"><i class="icon-social-instagram icons"></i></a>
-        <a href="https://twitter.com/ross_edin"><i class="icon-social-twitter icons"></i></a>
-          
+          <a class="aqua-icon" target="_blank" href="https://www.instagram.com/raspylines/"><i class="icon-social-instagram icons"></i></a>
+          <a class="coral-icon" target="_blank" href="https://twitter.com/ross_edin"><i class="icon-social-twitter icons"></i></a>         
         </div> 
       </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    {/* <Link to="/page-2/">Go to page 2</Link> */}
   </Layout>
 )
 
