dbe4a2309d33945d8486b3d5701d0e3787434e89
rhill <rhill@squiz.co.uk>
2020-06-24 14:15:36 +0100

    Added new project, some SCSS tweaks

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,10 +24,20 @@ const IndexPage = () => (
 
     <div class="projects">
       <p class="projects__title">Projects</p>
+      <div class="projects__info">
       {/* <Image /> */}
-      <img src="https://i.ibb.co/ysN90hW/etch.png" alt="Shows a logo for Etch, a web app"></img>
-      <p>Etch is a simple to use web app designed to simulate a life drawing class, using high-resolution photos from Unsplash</p>
-      <p><a target="_blank" href="https://rossghill.github.io/Etch/" rel="noreferrer">Try it out here!</a></p>
+        <img src="https://i.ibb.co/ysN90hW/etch.png" alt="Shows a logo for Etch, a web app"></img>
+        <p>Etch is a simple to use web app designed to simulate a life drawing class, using high-resolution photos from Unsplash</p>
+        <p><a target="_blank" href="https://rossghill.github.io/Etch/" rel="noreferrer">Try it out here</a></p>
+      </div>
+
+
+      <div class="projects__info">
+      {/* <Image /> */}
+      <a target="_blank" href="https://raspylines.myshopify.com/" rel="noreferrer"><img src="https://i.ibb.co/y4gCPH8/shop.png" alt="Represents Raspylines, an online store"></img></a>
+        <p>Personal online art store, built using Shopify/ SCSS and modified Liquid templates</p>
+        <p><a target="_blank" href="https://raspylines.myshopify.com/" rel="noreferrer">See it here</a></p>
+      </div>
     </div>
 
 
